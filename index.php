f77a3ab3559db949cdc4ba62db22e6bc9eba155c
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:21:39 +0100

    fini

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 4 Exercice 8</title>
+  </head>
+  <body>
+    <?php
+    $first = 1;
+    $second = 1;
+    $third = 1;
+    function sumNumber($nb1,$nb2,$nb3){
+      $result = $nb1 + $nb2 + $nb3;
+      return $result;
+    }
+    echo sumNumber($first,$second,$third);
+     ?>
+  </body>
+</html>
