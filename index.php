5b876871dae778b9039096e4c5e9f5ce3dc2bfbb
AndreVictor60 <andre.victor60@free.fr>
2017-12-19 14:53:50 +0100

    fini

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Partie 4 Exercice 8</title>
-  </head>
-  <body>
-    <?php
-    $first = 1;
-    $second = 1;
-    $third = 1;
-    function sumNumber($nb1,$nb2,$nb3){
-      $result = $nb1 + $nb2 + $nb3;
-      return $result;
-    }
-    echo sumNumber($first,$second,$third);
-     ?>
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <title>Partie 4 Exercice 8</title>
+    </head>
+    <body>
+        <?php
+
+        // Déclarer fonction avec les paramètres
+        function sumNumber($first = 1, $second = 1, $third = 1) {
+            //Additionner les variables
+            $result = $first + $second + $nb3;
+            return $result;
+        }
+
+        //Afficher & Appeler la fonction
+        echo sumNumber();
+        ?>
+    </body>
 </html>
